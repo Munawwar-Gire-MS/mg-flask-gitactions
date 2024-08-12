a66b8b1b8fd81bc821d0a33de1d67a812793c144
Munawwar-Gire-MS <v-mgire@microsoft.com>
2024-08-12 09:26:57 +0000

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,27 @@
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def home():
+    return 'I am Home Page'
+ 
+ 
+@app.route('/about')
+def about():
+    return 'I am about Page'
+ 
+ 
+@app.route('/help')
+def help():
+    return 'I am help Page'
+ 
+ 
+@app.route('/contact')
+def contact():
+    return 'I am contact Page'
+ 
+if __name__ == '__main__' :
+    app.run(debug=True)
+
+
